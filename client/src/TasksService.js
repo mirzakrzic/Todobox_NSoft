4394e314d880383d62ef376e3ad56879b3f33695
mirza.krzic <mirza.krzic@gmail.com>
2019-04-29 17:36:49 +0200

    some update on frotend

---
+++ client/src/TasksService.js
@@ -0,0 +1,20 @@
+import axios from axios
+
+const url = 'http://localhost:5000/api/tasks';
+
+class TaskService {
+    //Get tasks
+
+
+    //Get specific task
+
+
+    //Create task
+
+
+    //Update task
+
+
+    //Delete task
+    
+}
